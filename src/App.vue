a4c9dcef609b1e81d5598748de8a1776ee5a9057
camila012 <camila.ardila.ca@gmail.com>
2022-04-21 08:13:08 -0500

    Commit inicial

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div class="">
+      <Header/>
+    </div>
+    <div class="">
+       <Content/>
+    </div>
+    <div class="">
+      <Footer/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Content,
+    Footer
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="">
+    <h1>PokeApi</h1>
+  </div>
+</template>
